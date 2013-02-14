7a6e7b87e92e8a466f18b63158ac74d8f1f6081d
lazyppp <hino@gakko-net.co.jp>
2013-02-15 01:42:32 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTutorial::Application.config.secret_token = 'b97fc81863d5c533a479c6bd86bf51ec0d489f5e3cfc7e190fba1d2d35367a2275ae5cecc114ad8021431da5a172ba27ec14fdcf9275b8706b1319fdeead0e31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GitTutorial::Application.config.session_store :cookie_store, key: '_git_tutorial_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GitTutorial::Application.config.session_store :active_record_store
